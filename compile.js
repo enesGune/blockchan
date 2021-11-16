3c6931c84e274289d62296f367bfa209453e87c4
enes_guner <enes@gulenayva.com>
2021-11-16 17:10:39 +0300

    rrt

--- compile.js
+++ compile.js
@@ -7,5 +7,6 @@ const source = fs.readFileSync(inboxPath,'utf8');
 
 
 
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
 
-console.log(solc.compile(source,1));
+// console.log(solc.compile(source,1));
